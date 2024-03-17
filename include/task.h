aee0efbc13da24e4a7bf85cd0ef89c54d6fb1a96
jiashu-z <jiashu.zhang@uwaterloo.ca>
2024-03-17 00:34:10 -0400

    update iterative task & pr side task

--- include/task.h
+++ include/task.h
@@ -11,6 +11,11 @@
 #include <grpcpp/security/credentials.h>
 #include <cassert>
 #include "scheduler.h"
+#include <thread>
+#include <cassert>
+#include <map>
+#include <vector>
+#include <fstream>
 
 class BubbleBanditTask {
  protected:
@@ -21,9 +26,48 @@ class BubbleBanditTask {
   const BubbleBanditSchedulerClient scheduler_client_;
 
  public:
-  BubbleBanditTask(int64_t task_id, std::string name, std::string device, std::string scheduler_addr) : 
-  task_id_(task_id), name_(name), device_(device), scheduler_addr_(scheduler_addr), scheduler_client_(scheduler_addr) {
 
+  enum State {
+    SUBMITTED,
+    CREATED,
+    PAUSED,
+    RUNNING,
+    STOPPED,
+    FINISHED,
+  };
+
+  std::atomic<bool> init_event_;
+  std::atomic<bool> start_event_;
+  std::atomic<bool> pause_event_;
+  std::atomic<bool> stop_event_;
+  std::atomic<bool> preempt_event_;
+  std::thread runner_;
+  double duration_;
+  std::atomic<double> end_time_;
+  int profiler_level_;
+  std::map<std::string, std::vector<double>> time_records_;
+  State state_;
+
+  virtual auto submitted_to_created() -> void = 0;
+
+  virtual auto created_to_paused() -> void = 0;
+
+  virtual auto paused_to_running() -> void = 0;
+
+  virtual auto running_to_paused() -> void = 0;
+
+  virtual auto running_to_finished() -> void = 0;
+
+  virtual auto to_stopped() -> void = 0;
+
+  virtual auto is_finished() -> bool = 0;
+
+  virtual auto step() -> void = 0;
+
+  auto do_i_have_enough_time() -> bool {
+    auto current_time = get_current_time_in_micro();
+
+    return end_time_ - current_time > duration_;
   }
 
   auto get_current_time_in_micro() -> double {
@@ -37,38 +81,191 @@ class BubbleBanditTask {
     return static_cast<double>(microseconds) / 1000000.0;
   }
 
-  virtual int64_t init(int64_t task_id) = 0;
+  auto record_time(const std::string &action) -> void {
+    if (time_records_.find(action) != time_records_.end()) {
+      time_records_[action].push_back(get_current_time_in_micro());
+    } else {
+      auto v = std::vector<double>();
+      v.push_back(get_current_time_in_micro());
+      time_records_.insert(std::make_pair(action, v));
+    }
+  }
 
-  virtual int64_t start(int64_t task_id, double end_time) = 0;
+  BubbleBanditTask(int64_t task_id,
+                   std::string name,
+                   std::string device,
+                   std::string scheduler_addr,
+                   int profiler_level) :
+      task_id_(task_id),
+      name_(name),
+      device_(device),
+      scheduler_addr_(scheduler_addr),
+      scheduler_client_(scheduler_addr) {
+    init_event_ = false;
+    start_event_ = false;
+    pause_event_ = false;
+    stop_event_ = false;
+    preempt_event_ = false;
+    profiler_level_ = profiler_level;
+    state_ = SUBMITTED;
+    end_time_ = -1;
+  }
 
-  virtual int64_t pause(int64_t task_id) = 0;
+  virtual int64_t init(int64_t task_id) {
+    assert(task_id == task_id_);
+    printf("Init task %ld\n", task_id_);
+    init_event_ = true;
+    return 0;
+  }
 
-  virtual int64_t stop(int64_t task_id) = 0;
+  virtual int64_t start(int64_t task_id, double end_time) {
+    assert(task_id == task_id_);
+    printf("Start task %ld\n", task_id_);
+    start_event_ = true;
+    end_time_ = end_time;
+    return 0;
+  }
 
-  virtual int64_t preempt(int64_t task_id) = 0;
+  virtual int64_t pause(int64_t task_id) {
+    assert(task_id == task_id_);
+    printf("Pause task %ld\n", task_id_);
+    pause_event_ = true;
+    return 0;
+  }
 
-  virtual void run() = 0;
+  virtual int64_t stop(int64_t task_id) {
+    assert(task_id == task_id_);
+    printf("Stop task %ld\n", task_id_);
+    stop_event_ = true;
+    return 0;
+  }
 
-  virtual void finish() = 0;
+  virtual int64_t preempt(int64_t task_id) {
+    assert(task_id == task_id_);
+    printf("Preempt task %ld\n", task_id_);
+    preempt_event_ = true;
+    return 0;
+  }
 
-  virtual void start_runner() = 0;
+  virtual void finish() {
+    auto ret = scheduler_client_.finish_task(task_id_);
+    assert(ret == 0);
+    printf("Finish task %ld\n", task_id_);
+  }
 
-  enum State {
-    SUBMITTED,
-    CREATED,
-    PENDING,
-    RUNNING,
-  };
+  virtual void run() {
+    record_time("SUBMITTED_TO_CREATED_START");
+    submitted_to_created();
+    state_ = CREATED;
+    record_time("SUBMITTED_TO_CREATED_END");
+    printf("State from SUBMITTED to CREATED\n");
+
+    while (true) {
+      switch (state_) {
+        case CREATED: {
+          if (init_event_) {
+            init_event_ = false;
+            record_time("CREATED_TO_PAUSED_START");
+            created_to_paused();
+            state_ = PAUSED;
+            record_time("CREATED_TO_PAUSED_END");
+            printf("State from CREATED to PAUSED\n");
+          }
+          break;
+        }
+        case PAUSED: {
+          if (start_event_) {
+            start_event_ = false;
+            record_time("PAUSED_TO_RUNNING_START");
+            paused_to_running();
+            state_ = RUNNING;
+            record_time("PAUSED_TO_RUNNING_END");
+            printf("State from PAUSED to RUNNING\n");
+          }
+          break;
+        }
+        case RUNNING: {
+          if (pause_event_) {
+            pause_event_ = false;
+            record_time("RUNNING_TO_PAUSED_START");
+            running_to_paused();
+            state_ = PAUSED;
+            record_time("RUNNING_TO_PAUSED_END");
+            printf("State from RUNNING to PAUSED\n");
+          }
+          if (is_finished()) {
+            record_time("RUNNING_TO_FINISHED_START");
+            running_to_finished();
+            state_ = FINISHED;
+            record_time("RUNNING_TO_FINISHED_END");
+            printf("State from RUNNING to FINISHED\n");
+            goto LOOP_END;
+          }
+          if (do_i_have_enough_time()) {
+            record_time("RUNNING_STEP_START");
+            step();
+            record_time("RUNNING_STEP_END");
+          } else {
+            record_time("RUNNING_STEP_START");
+            if (end_time_.load() - get_current_time_in_micro() > 0.001) {
+              usleep(end_time_.load() - get_current_time_in_micro());
+            }
+            record_time("RUNNING_STEP_END");
+          }
+          break;
+        }
+        default: {
+          assert(false);
+        }
+      }
+      if (stop_event_) {
+        record_time("TO_STOPPED_START");
+        to_stopped();
+        state_ = STOPPED;
+        record_time("TO_STOPPED_END");
+        printf("State to STOPPED\n");
+      }
+      usleep(1000);
+    }
+
+    LOOP_END:
+
+    std::ofstream out_file;
+    std::string out_file_name = name_ + "_time_profile.txt";
+    out_file.open(out_file_name);
+    assert(out_file.is_open());
+    for (auto &kv : time_records_) {
+      out_file << kv.first << ",";
+      for (auto &v : kv.second) {
+        out_file << v << ",";
+      }
+      out_file << std::endl;
+    }
+    out_file.flush();
+    out_file.close();
+
+    if (stop_event_) {
+      stop_event_ = false;
+    } else {
+      finish();
+    }
+  }
+
+  virtual void start_runner() {
+    printf("Start runner of task %ld\n", task_id_);
+    runner_ = std::thread([this] { run(); });
+  }
+
 };
 
 class TaskServiceImpl final : public Task::Service {
  private:
-  BubbleBanditTask * const task_;
+  BubbleBanditTask *const task_;
  public:
   TaskServiceImpl(BubbleBanditTask *task) : task_(task) {
   }
 
-  grpc::Status InitTask(grpc::ServerContext* context, const InitTaskArgs* request, InitTaskReply* response) override {
+  grpc::Status InitTask(grpc::ServerContext *context, const InitTaskArgs *request, InitTaskReply *response) override {
     auto task_id = request->task_id();
     auto status = task_->init(task_id);
     if (status == 0) {
@@ -78,7 +275,9 @@ class TaskServiceImpl final : public Task::Service {
     }
   }
 
-  grpc::Status StartTask(grpc::ServerContext* context, const StartTaskArgs* request, StartTaskReply* response) override {
+  grpc::Status StartTask(grpc::ServerContext *context,
+                         const StartTaskArgs *request,
+                         StartTaskReply *response) override {
     auto task_id = request->task_id();
     auto end_time = request->end_time();
     auto status = task_->start(task_id, end_time);
@@ -89,7 +288,9 @@ class TaskServiceImpl final : public Task::Service {
     }
   }
 
-  grpc::Status PauseTask(grpc::ServerContext* context, const PauseTaskArgs* request, PauseTaskReply* response) override {
+  grpc::Status PauseTask(grpc::ServerContext *context,
+                         const PauseTaskArgs *request,
+                         PauseTaskReply *response) override {
     auto task_id = request->task_id();
     auto status = task_->pause(task_id);
     if (status == 0) {
@@ -99,7 +300,7 @@ class TaskServiceImpl final : public Task::Service {
     }
   }
 
-  grpc::Status StopTask(grpc::ServerContext* context, const StopTaskArgs* request, StopTaskReply* response) override {
+  grpc::Status StopTask(grpc::ServerContext *context, const StopTaskArgs *request, StopTaskReply *response) override {
     auto task_id = request->task_id();
     auto status = task_->stop(task_id);
     if (status == 0) {
@@ -109,7 +310,9 @@ class TaskServiceImpl final : public Task::Service {
     }
   }
 
-  grpc::Status PreemptTask(grpc::ServerContext* context, const PreemptTaskArgs* request, PreemptTaskReply* response) override {
+  grpc::Status PreemptTask(grpc::ServerContext *context,
+                           const PreemptTaskArgs *request,
+                           PreemptTaskReply *response) override {
     auto task_id = request->task_id();
     auto status = task_->preempt(task_id);
     if (status == 0) {
--- src/pr/pr_side_task.cu
+++ src/pr/pr_side_task.cu
@@ -2,21 +2,18 @@
 #include <iostream>
 #include "task.h"
 #include <thread>
-#include <unistd.h>
 #include <csignal>
 #include <grpcpp/security/server_credentials.h>
 #include <grpcpp/server.h>
 #include <grpcpp/server_builder.h>
 #include <atomic>
 #include "cutil_subset.h"
-#include "worklistc.h"
-#include <chrono>
 
 #include "pr.h"
-#include "timer.h"
-#include "cutil_subset.h"
 #include "cuda_launch_config.hpp"
 #include <cub/cub.cuh>
+#include <fstream>
+#include <cassert>
 
 #define FUSED 0
 #define PR_VARIANT "pull"
@@ -61,316 +58,142 @@ __global__ void l1norm(int m, ScoreT *scores, ScoreT *sums,
   if (threadIdx.x == 0) atomicAdd(diff, block_sum);
 }
 
-// pull operation needs incoming neighbor list
-__global__ void pull_fused(int m, const uint64_t *row_offsets,
-                           const VertexId *column_indices,
-                           ScoreT *scores, ScoreT *outgoing_contrib,
-                           float *diff, ScoreT base_score) {
-  typedef cub::BlockReduce<float, BLOCK_SIZE> BlockReduce;
-  int src = blockIdx.x * blockDim.x + threadIdx.x;
-  __shared__ typename BlockReduce::TempStorage temp_storage;
-  float error = 0;
-  if (src < m) {
-    IndexT row_begin = row_offsets[src];
-    IndexT row_end = row_offsets[src + 1];
-    ScoreT incoming_total = 0;
-    for (IndexT offset = row_begin; offset < row_end; ++offset) {
-      IndexT dst = column_indices[offset];
-      incoming_total += outgoing_contrib[dst];
-    }
-    ScoreT old_score = scores[src];
-    scores[src] = base_score + kDamp * incoming_total;
-    error += fabs(scores[src] - old_score);
-  }
-  float block_sum = BlockReduce(temp_storage).Sum(error);
-  if (threadIdx.x == 0) atomicAdd(diff, block_sum);
-}
-
-void PRSolver(Graph &g, ScoreT *scores) {
-  auto m = g.V();
-  auto nnz = g.E();
-  auto h_row_offsets = g.in_rowptr();
-  auto h_column_indices = g.in_colidx();
-  //print_device_info(0);
-  uint64_t *d_row_offsets;
-  VertexId *d_column_indices;
-  CUDA_SAFE_CALL(cudaMalloc((void **) &d_row_offsets, (m + 1) * sizeof(uint64_t)));
-  CUDA_SAFE_CALL(cudaMalloc((void **) &d_column_indices, nnz * sizeof(VertexId)));
-  CUDA_SAFE_CALL(cudaMemcpy(d_row_offsets, h_row_offsets, (m + 1) * sizeof(uint64_t), cudaMemcpyHostToDevice));
-  CUDA_SAFE_CALL(cudaMemcpy(d_column_indices, h_column_indices, nnz * sizeof(VertexId), cudaMemcpyHostToDevice));
-
-  std::vector<VertexId> degrees(m);
-  for (VertexId i = 0; i < m; i++) degrees[i] = g.get_degree(i);
-  int *d_degrees;
-  CUDA_SAFE_CALL(cudaMalloc((void **) &d_degrees, m * sizeof(int)));
-  CUDA_SAFE_CALL(cudaMemcpy(d_degrees, &degrees[0], m * sizeof(int), cudaMemcpyHostToDevice));
-  ScoreT *d_scores, *d_sums, *d_contrib;
-  CUDA_SAFE_CALL(cudaMalloc((void **) &d_scores, m * sizeof(ScoreT)));
-  CUDA_SAFE_CALL(cudaMalloc((void **) &d_sums, m * sizeof(ScoreT)));
-  CUDA_SAFE_CALL(cudaMalloc((void **) &d_contrib, m * sizeof(ScoreT)));
-  CUDA_SAFE_CALL(cudaMemcpy(d_scores, scores, m * sizeof(ScoreT), cudaMemcpyHostToDevice));
-  float *d_diff, h_diff;
-  CUDA_SAFE_CALL(cudaMalloc((void **) &d_diff, sizeof(float)));
-
-  int iter = 0;
-  int nthreads = BLOCK_SIZE;
-  int nblocks = (m - 1) / nthreads + 1;
-  const ScoreT base_score = (1.0f - kDamp) / m;
-  printf("Launching CUDA PR solver (%d CTAs, %d threads/CTA) ...\n", nblocks, nthreads);
-
-  Timer t;
-  t.Start();
-  do {
-    ++iter;
-    h_diff = 0;
-    CUDA_SAFE_CALL(cudaMemcpy(d_diff, &h_diff, sizeof(float), cudaMemcpyHostToDevice));
-    contrib <<<nblocks, nthreads>>>(m, d_scores, d_degrees, d_contrib);
-    CudaTest("solving kernel contrib failed");
-#if FUSED
-    pull_fused<<<nblocks, nthreads>>>(m, d_row_offsets, d_column_indices, d_scores, d_contrib, d_diff, base_score);
-#else
-    pull_step<<<nblocks, nthreads>>>(m, d_row_offsets, d_column_indices, d_sums, d_contrib);
-    l1norm<<<nblocks, nthreads>>>(m, d_scores, d_sums, d_diff, base_score);
-#endif
-    CudaTest("solving kernel pull failed");
-    CUDA_SAFE_CALL(cudaMemcpy(&h_diff, d_diff, sizeof(float), cudaMemcpyDeviceToHost));
-    printf(" %2d    %f\n", iter, h_diff);
-  } while (h_diff > EPSILON && iter < MAX_ITER);
-  CUDA_SAFE_CALL(cudaDeviceSynchronize());
-  t.Stop();
-
-  printf("\titerations = %d.\n", iter);
-  printf("\truntime [cuda_pull] = %f ms.\n", t.Millisecs());
-  CUDA_SAFE_CALL(cudaMemcpy(scores, d_scores, m * sizeof(ScoreT), cudaMemcpyDeviceToHost));
-  CUDA_SAFE_CALL(cudaFree(d_row_offsets));
-  CUDA_SAFE_CALL(cudaFree(d_column_indices));
-  CUDA_SAFE_CALL(cudaFree(d_degrees));
-  CUDA_SAFE_CALL(cudaFree(d_scores));
-  CUDA_SAFE_CALL(cudaFree(d_sums));
-  CUDA_SAFE_CALL(cudaFree(d_contrib));
-  CUDA_SAFE_CALL(cudaFree(d_diff));
-  return;
-}
-
 class PrSideTask final : public BubbleBanditTask {
  private:
 
-  bool with_profiler_;
   std::string file_type_;
   std::string graph_prefix_;
   std::string symmetrize_;
-  std::atomic<bool> init_event_;
-  std::atomic<bool> start_event_;
-  std::atomic<bool> pause_event_;
-  std::atomic<bool> stop_event_;
-  std::atomic<bool> preempt_event_;
-  std::atomic<int> counter_;
-  std::atomic<int64_t> ts0_;
-  std::atomic<int64_t> ts1_;
-  std::thread runner_;
-  double duration_;
-  std::atomic<double> end_time_;
+  int max_iter_;
 
-  auto do_i_have_enough_time() -> bool {
-    auto current_time = get_current_time_in_micro();
+  Graph *g_ptr;
+  VertexId m;
+  size_t nnz;
+  uint64_t *h_row_offsets;
+  VertexId *h_column_indices;
+  uint64_t *d_row_offsets;
+  VertexId *d_column_indices;
+  int *d_degrees;
+  ScoreT *d_scores, *d_sums, *d_contrib;
+  float *d_diff, h_diff;
 
-    return end_time_ - current_time > duration_;
-  }
+  std::vector<VertexId> degrees;
+  std::atomic<int> iter;
+  int nthreads;
+  int nblocks;
+  ScoreT base_score;
+  ScoreT init_score;
+  std::vector<ScoreT> scores_vec;
+  float *scores;
 
  public:
   PrSideTask(int task_id,
              std::string name,
              std::string device,
              std::string scheduler_addr,
-             bool with_profiler,
+             int profiler_level,
              std::string file_type,
              std::string graph_prefix,
-             std::string symmetrize) : BubbleBanditTask(task_id, name, device, scheduler_addr) {
-    with_profiler_ = with_profiler;
+             std::string symmetrize,
+             int max_iter) : BubbleBanditTask(task_id, name, device, scheduler_addr, profiler_level) {
     file_type_ = file_type;
     graph_prefix_ = graph_prefix;
     symmetrize_ = symmetrize;
-    init_event_ = false;
-    start_event_ = false;
-    pause_event_ = false;
-    stop_event_ = false;
-    preempt_event_ = false;
-    counter_ = 0;
-    ts0_ = 0;
-    ts1_ = 0;
+    max_iter_ = max_iter;
     duration_ = 0.1;
-    end_time_ = 0.0;
   }
 
-  int64_t init(int64_t task_id) override {
-    assert(task_id == task_id_);
-    std::cout << "Init task " << task_id << std::endl;
-    init_event_ = true;
-    return 0;
-  }
-
-  int64_t start(int64_t task_id, double end_time) override {
-    assert(task_id == task_id_);
-    std::cout << "Start task " << task_id << " with end time " << end_time << std::endl;
-    end_time_ = end_time;
-    start_event_ = true;
-    return 0;
-  }
-
-  int64_t pause(int64_t task_id) override {
-    assert(task_id == task_id_);
-    std::cout << "Pause task " << task_id << std::endl;
-    pause_event_ = true;
-    return 0;
-  }
-
-  int64_t stop(int64_t task_id) override {
-    assert(task_id == task_id_);
-    std::cout << "Stop task " << task_id << std::endl;
-    stop_event_ = true;
-    runner_.join();
-    std::cout << "Task " << task_id << " stopped" << std::endl;
-    kill(getpid(), SIGINT);
-    return 0;
-  }
-
-  int64_t preempt(int64_t task_id) override {
-    assert(task_id == task_id_);
-    std::cout << "Preempt task " << task_id << std::endl;
-    preempt_event_ = true;
-    return 0;
-  }
-
-  void run() override {
+  auto submitted_to_created() -> void override {
     auto device = device_.at(5) - '0';
-    std::cout << "Device: " << device << std::endl;
+    printf("CUDA:%d\n", device);
     cudaSetDevice(device);
-    if (with_profiler_) {
-//      TODO
-    }
-    Graph g(graph_prefix_, file_type_, std::stoi(symmetrize_));
-    auto m = g.V();
-    auto nnz = g.E();
-    auto h_row_offsets = g.in_rowptr();
-    auto h_column_indices = g.in_colidx();
-    uint64_t *d_row_offsets;
-    VertexId *d_column_indices;
-    int *d_degrees;
-    ScoreT *d_scores, *d_sums, *d_contrib;
-    float *d_diff, h_diff;
+    g_ptr = new Graph(graph_prefix_, file_type_, std::stoi(symmetrize_));
+    auto &g = *g_ptr;
+    m = g.V();
+    nnz = g.E();
+    h_row_offsets = g.in_rowptr();
+    h_column_indices = g.in_colidx();
 
-    std::vector<VertexId> degrees(m);
+    degrees = std::vector<VertexId>(m);
     for (VertexId i = 0; i < m; i++) {
       degrees[i] = g.get_degree(i);
     }
-    int iter = 0;
-    int nthreads = BLOCK_SIZE;
-    int nblocks = (m - 1) / nthreads + 1;
-    const ScoreT base_score = (1.0f - kDamp) / m;
-    const ScoreT init_score = 1.0f / m;
-    std::vector<ScoreT> scores_vec(m, init_score);
-    auto scores = &scores_vec[0];
+    iter = 0;
+    nthreads = BLOCK_SIZE;
+    nblocks = (m - 1) / nthreads + 1;
+    base_score = (1.0f - kDamp) / m;
+    init_score = 1.0f / m;
+    scores_vec = std::vector<ScoreT>(m, init_score);
+    scores = &scores_vec[0];
     printf("Launching CUDA PR solver (%d CTAs, %d threads/CTA) ...\n", nblocks, nthreads);
 
-    auto state = BubbleBanditTask::State::CREATED;
-
-    while (true) {
-      switch (state) {
-        case BubbleBanditTask::State::CREATED: {
-          if (init_event_) {
-            init_event_ = false;
-            CUDA_SAFE_CALL(cudaMalloc((void **) &d_row_offsets, (m + 1) * sizeof(uint64_t)));
-            CUDA_SAFE_CALL(cudaMalloc((void **) &d_column_indices, nnz * sizeof(VertexId)));
-            CUDA_SAFE_CALL(cudaMemcpy(d_row_offsets,
-                                      h_row_offsets,
-                                      (m + 1) * sizeof(uint64_t),
-                                      cudaMemcpyHostToDevice))
-            CUDA_SAFE_CALL(cudaMemcpy(d_column_indices,
-                                      h_column_indices,
-                                      nnz * sizeof(VertexId),
-                                      cudaMemcpyHostToDevice))
-
-            CUDA_SAFE_CALL(cudaMalloc((void **) &d_degrees, m * sizeof(int)));
-            CUDA_SAFE_CALL(cudaMemcpy(d_degrees, &degrees[0], m * sizeof(int), cudaMemcpyHostToDevice));
-            CUDA_SAFE_CALL(cudaMalloc((void **) &d_scores, m * sizeof(ScoreT)));
-            CUDA_SAFE_CALL(cudaMalloc((void **) &d_sums, m * sizeof(ScoreT)));
-            CUDA_SAFE_CALL(cudaMalloc((void **) &d_contrib, m * sizeof(ScoreT)));
-            CUDA_SAFE_CALL(cudaMemcpy(d_scores, scores, m * sizeof(ScoreT), cudaMemcpyHostToDevice));
-            CUDA_SAFE_CALL(cudaMalloc((void **) &d_diff, sizeof(float)));
-          }
-          break;
-        }
-        case BubbleBanditTask::State::PENDING: {
-          if (start_event_) {
-            start_event_ = false;
-            state = BubbleBanditTask::State::RUNNING;
-            std::cout << "State from PENDING to RUNNING" << std::endl;
-          } else if (preempt_event_) {
-            preempt_event_ = false;
-          }
-          break;
-        }
-        case BubbleBanditTask::State::RUNNING: {
-          if (pause_event_) {
-            pause_event_ = false;
-            state = BubbleBanditTask::State::PENDING;
-            std::cout << "State from RUNNING to PENDING" << std::endl;
-          } else if (preempt_event_) {
-            preempt_event_ = false;
-          } else {
-            if (!do_i_have_enough_time()) {
-              printf("I do not have enough time, current: %f, end: %f\n",
-                     get_current_time_in_micro(),
-                     end_time_.load());
-              auto end_time = end_time_.load();
-              if (end_time - get_current_time_in_micro() > 1000) {
-                usleep((end_time - get_current_time_in_micro()) / 1000);
-              }
-            } else {
-              ++iter;
-              h_diff = 0;
-              CUDA_SAFE_CALL(cudaMemcpy(d_diff, &h_diff, sizeof(float), cudaMemcpyHostToDevice));
-              contrib <<<nblocks, nthreads>>>(m, d_scores, d_degrees, d_contrib);
-              CudaTest("solving kernel contrib failed");
-              pull_step<<<nblocks, nthreads>>>(m, d_row_offsets, d_column_indices, d_sums, d_contrib);
-              l1norm<<<nblocks, nthreads>>>(m, d_scores, d_sums, d_diff, base_score);
-              CudaTest("solving kernel pull failed");
-              CUDA_SAFE_CALL(cudaMemcpy(&h_diff, d_diff, sizeof(float), cudaMemcpyDeviceToHost));
-              printf(" %2d    %f\n", iter, h_diff);
-              if (!(h_diff > EPSILON && iter < MAX_ITER)) {
-                goto BREAK_LOOP;
-              }
-            }
-          }
-          break;
-        }
-        default: {
-          assert(false);
-        }
-      }
-      if (stop_event_) {
-        break;
-      }
-      usleep(10000);
-    }
-    BREAK_LOOP:
-    if (with_profiler_) {
-//    TODO
-    }
-    if (stop_event_) {
-      stop_event_ = false;
-    } else {
-      scheduler_client_.finish_task(task_id_);
-    }
+    state_ = BubbleBanditTask::State::CREATED;
   }
 
-  void finish() override {
+  auto created_to_paused() -> void override {
+    CUDA_SAFE_CALL(cudaMalloc((void **) &d_row_offsets, (m + 1) * sizeof(uint64_t)));
+    CUDA_SAFE_CALL(cudaMalloc((void **) &d_column_indices, nnz * sizeof(VertexId)));
+    CUDA_SAFE_CALL(cudaMemcpy(d_row_offsets,
+                              h_row_offsets,
+                              (m + 1) * sizeof(uint64_t),
+                              cudaMemcpyHostToDevice))
+    CUDA_SAFE_CALL(cudaMemcpy(d_column_indices,
+                              h_column_indices,
+                              nnz * sizeof(VertexId),
+                              cudaMemcpyHostToDevice))
 
+    CUDA_SAFE_CALL(cudaMalloc((void **) &d_degrees, m * sizeof(int)));
+    CUDA_SAFE_CALL(cudaMemcpy(d_degrees, &degrees[0], m * sizeof(int), cudaMemcpyHostToDevice));
+    CUDA_SAFE_CALL(cudaMalloc((void **) &d_scores, m * sizeof(ScoreT)));
+    CUDA_SAFE_CALL(cudaMalloc((void **) &d_sums, m * sizeof(ScoreT)));
+    CUDA_SAFE_CALL(cudaMalloc((void **) &d_contrib, m * sizeof(ScoreT)));
+    CUDA_SAFE_CALL(cudaMemcpy(d_scores, scores, m * sizeof(ScoreT), cudaMemcpyHostToDevice));
+    CUDA_SAFE_CALL(cudaMalloc((void **) &d_diff, sizeof(float)));
   }
 
-  void start_runner() override {
-
+  auto paused_to_running() -> void override {
   }
+
+  auto running_to_paused() -> void override {
+  }
+
+  auto running_to_finished() -> void override {
+  }
+
+/*
+
+    def to_stopped(self) -> None:
+        with open(f"./{self.task_name}_{self.task_id}_side_task.txt", "w") as f:
+            f.write(str(self.step_counter * self.batch_size))
+            f.flush()
+*/
+
+  auto to_stopped() -> void override {
+    std::string out_file_name = name_ + "_" + std::to_string(task_id_) + "_side_task.txt";
+    std::ofstream out_file(out_file_name);
+    assert(out_file.is_open());
+    out_file << iter;
+    out_file.flush();
+    out_file.close();
+  }
+
+  auto is_finished() -> bool override {
+    return iter >= max_iter_;
+  }
+
+  auto step() -> void override {
+    ++iter;
+    h_diff = 0;
+    CUDA_SAFE_CALL(cudaMemcpy(d_diff, &h_diff, sizeof(float), cudaMemcpyHostToDevice));
+    contrib <<<nblocks, nthreads>>>(m, d_scores, d_degrees, d_contrib);
+    CudaTest("solving kernel contrib failed");
+    pull_step<<<nblocks, nthreads>>>(m, d_row_offsets, d_column_indices, d_sums, d_contrib);
+    l1norm<<<nblocks, nthreads>>>(m, d_scores, d_sums, d_diff, base_score);
+    CudaTest("solving kernel pull failed");
+    CUDA_SAFE_CALL(cudaMemcpy(&h_diff, d_diff, sizeof(float), cudaMemcpyDeviceToHost));
+    printf(" %2d    %f\n", iter.load(), h_diff);
+  }
+
 };
 
 grpc::Server *server_ptr;
@@ -392,11 +215,11 @@ int main(int argc, char **argv) {
   program.add_argument("-i", "--task_id");
   program.add_argument("-d", "--device");
   program.add_argument("-a", "--addr");
-  // TODO: Jiashu: Fix profiler flag
-  // program.add_argument("-p", "--profiler");
+  program.add_argument("--profiler_level");
   program.add_argument("-t", "--file_type");
   program.add_argument("-g", "--graph_prefix");
   program.add_argument("--symmetrize");
+  program.add_argument("--max_iter");
   try {
     program.parse_args(argc, argv);
   } catch (const std::runtime_error &err) {
@@ -410,13 +233,14 @@ int main(int argc, char **argv) {
   auto task_id = std::stoi(program.get<std::string>("--task_id"));
   auto device = program.get<std::string>("--device");
   auto addr = program.get<std::string>("--addr");
-  // auto with_profiler = bool(std::stoi(program.get<std::string>("--profiler")));
-  auto with_profiler = false;
+  auto profiler_level = program.get<int>("--profiler_level");
   auto file_type = program.get<std::string>("--file_type");
   auto graph_prefix = program.get<std::string>("--graph_prefix");
   auto symmetrize = program.get<std::string>("--symmetrize");
+  auto max_iter = program.get<int>("--max_iter");
 
-  auto task = PrSideTask(task_id, name, device, scheduler_addr, with_profiler, file_type, graph_prefix, symmetrize);
+  auto task =
+      PrSideTask(task_id, name, device, scheduler_addr, profiler_level, file_type, graph_prefix, symmetrize, max_iter);
 
   auto service = TaskServiceImpl(&task);
   grpc::ServerBuilder builder;
